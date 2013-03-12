e496b0cdb43a9a43c9f0e2362c7dd36dfe835a11
Maxdweb <deleuil.maxime@gmail.com>
2013-03-12 21:27:43 +0100

    Update : Ajout de la deconnexion

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -9,24 +9,27 @@ switch($action){
 		break;
 	}
 	case 'valideConnexion':{
-		$login = $_REQUEST['login'];
-		$mdp = $_REQUEST['mdp'];
-		$visiteur = $pdo->getInfosVisiteur($login,$mdp);
+		$visiteur = $pdo->getInfosVisiteur($_REQUEST['login'],$_REQUEST['mdp']);
 		if(!is_array( $visiteur)){
 			ajouterErreur("Login ou mot de passe incorrect");
 			include("vues/v_erreurs.php");
 			include("vues/v_connexion.php");
 		}
 		else{
-			$id = $visiteur['VIS_MATRICULE'];
-			$nom =  $visiteur['VIS_NOM'];
-			$prenom = $visiteur['VIS_PRENOM'];
-                        $_SESSION['login']= $login; // mémorise les variables de session
-			$_SESSION['vis_matricule']= $id;
-                        $_SESSION['nom']= $nom;
-                        $_SESSION['prenom']= $prenom;
-                        include("vues/v_sommaire.php");
+            $_SESSION['login']= $_REQUEST['login']; // mémorise les variables de session
+			$_SESSION['vis_matricule']= $visiteur['VIS_MATRICULE'];
+            $_SESSION['nom']= $visiteur['VIS_NOM'];
+            $_SESSION['prenom']= $visiteur['VIS_PRENOM'];
+            include("vues/v_sommaire.php");
 			}
+		break;
+	}
+	case 'deconnexion' :{
+		unset($_SESSION['login']);
+		unset($_SESSION['vis_matricule']);
+		unset($_SESSION['nom']);
+		unset($_SESSION['prenom']);
+		include("vues/v_connexion.php");
 		break;
 	}
 	default :{
