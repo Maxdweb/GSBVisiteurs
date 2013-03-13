0934a31c466876654309007411ef06978c42c139
Maxdweb <deleuil.maxime@gmail.com>
2013-03-13 21:08:17 +0100

    UPDATE : liens + mise en page du sommaire

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -6,27 +6,31 @@
     </h2>
           </div>  
         <ul id="menuList">
-			<li >
-				 Bienvenue :<br>
-				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
-			</li>
-              <li class="smenu">
-                   Comptes rendus 
+      			<li >
+      				 Bienvenue :<br>
+      				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
+      			</li><br/>
+
+           <li class="smenu">
+              Comptes rendus 
            </li> 
            <li class="smenu">
-              <a href="index.php?uc=gererCR&action=saisirCR=" title="Nouveaux comptes rendus">Nouveaux</a>
+              <a href="index.php?uc=gererCR&action=saisirCR" title="Nouveaux comptes rendus">Nouveaux</a>
            </li>
            <li class="smenu">
-              <a href="index.php?uc=" title="Consulter les comptes rendus">Consulter</a>
-           </li><li class="smenu">
+              <a href="index.php?uc=gererCR&action=consulterCR" title="Consulter les comptes rendus">Consulter</a>
+           </li><br/>
+
            <li class="smenu">
               Consultations
            </li>
            <li class="smenu">
-              <a href="index.php?uc=" title="Consulter les médicaments">    Médicaments</a>
-           </li><li class="smenu">
-              <a href="index.php?uc=" title="Consulter les praticiens">Praticiens</a>
+              <a href="index.php?uc=consulterMedicaments" title="Consulter les médicaments">    Médicaments</a>
            </li>
+           <li class="smenu">
+              <a href="index.php?uc=consulterMedicaments" title="Consulter les praticiens">Praticiens</a>
+           </li><br/>
+
            <li class="smenu">
               <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
            </li>
