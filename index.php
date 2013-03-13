050757f50cc709aeab11c5bb0afc8b789e68cf9d
Maxdweb <deleuil.maxime@gmail.com>
2013-03-13 21:07:29 +0100

    PATCH : conditions sur UC var + UPDATE : cas du switch

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php
 require_once("include/fct.inc.php");
 require_once ("include/modele.inc.php");
+
 include("vues/v_entete.php") ;
+
 session_start();
 $pdo = PdoGsb::getPdoGsb();
-if(!isset($_REQUEST['uc']) || (!isset($_SESSION['login']))){
-     $_REQUEST['uc'] = 'connexion';
-}	 
-$uc = $_REQUEST['uc'];
+
+if(!isset($_REQUEST['uc']) && (!isset($_SESSION['login'])))
+    $uc = 'connexion';
+else if(!isset($_REQUEST['uc']) && (isset($_SESSION['login'])))
+	$uc = '';
+else
+	$uc = $_REQUEST['uc'];
+
 switch($uc){
 	case 'connexion':{
 		include("controleurs/c_connexion.php");break;
 	}
-	case 'gererFrais' :{
-		include("controleurs/c_gererFrais.php");break;
+	case 'gererCR':{
+		include("controleurs/c_gererCR.php");break;
 	}
-	case 'etatFrais' :{
-		include("controleurs/c_etatFrais.php");break; 
+	case 'consulterMedicaments' :{
+		include("controleurs/c_consulterMedicaments.php");break;
+	}
+	case 'consulterPratictiens' :{
+		include("controleurs/c_consulterPratictiens.php");break; 
+	}
+	default :{
+		include("vues/v_sommaire.php");
+		include("vues/v_accueil.php");
+		break;
 	}
 }
+
 include("vues/v_pied.php") ;
 ?>
