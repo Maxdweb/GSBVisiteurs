26e0b87dd619f439aa28f59c43c2814adb84cf23
Guemas anthony <guemas12.anthony@gmail.com>
2013-03-15 09:56:04 +0100

    controleur et vue praticien    Version 1

--- controleurs/c_consulterPraticiens.php
+++ controleurs/c_consulterPraticiens.php
@@ -1,7 +1,18 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+    // affichage de l'en-tete
+    include('v_entete.php');
+    include('modele.inc.php');
+    
+    // selection du tri du tableau (par defaut -> id)
+    if( !isset($_REQUEST['tri']))
+        $tri = "PRA_NUM";
+    else 
+        $tri = $_REQUEST['tri'];
+    
+    // recuperation des informations
+    $praticiens = getDetailPraticiens($tri);
+    
+    include('v_praticiens.php');
+    
 ?>
---
+++ vues/v_praticien.php
@@ -0,0 +1,55 @@
+<!-- Affichage du tableau de consultation des praticiens  -->
+
+
+<table>
+    <!-- Affichage des titres -->
+    <tr>
+        <th>
+            <a href ="index.php?uc=consulterPratictiens&tri=PRA_NUM" > Numero </a>
+        </th>
+        <th>
+             <a href ="index.php?uc=consulterPratictiens&tri=PRA_NOM" > Nom </a>
+        </th>
+        <th>
+             <a href ="index.php?uc=consulterPratictiens&tri=PRA_PRENOM" > Prenom </a>
+        <!-- Adresse, code postal, ville -->
+        <th>
+             <a href ="index.php?uc=consulterPratictiens&tri=PRA_ADRESSE" > Adresse </a>
+        </th>
+        <th>
+             <a href ="index.php?uc=consulterPratictiens&tri=PRA_COEFNOTORIETE" > Notori&eacute;t&eacute; </a>
+        </th>
+        <th>
+             <a href ="index.php?uc=consulterPratictiens&tri=TYP_CODE" > Code </a>
+        </th>
+    </tr>   
+
+<?php
+    // parcours du tableau 
+    foreach($praticiens as $unPraticien)
+    {
+?>
+        
+    <tr>
+        <td>
+            <?php $unPraticien['PRA_NUM'] ?>
+        </td>
+        <td>
+            <?php $unPraticien['PRA_NOM'] ?>
+        </td>
+        <td>
+            <?php $unPraticien['PRA_PRENOM'] ?>
+        </td>
+        <td>
+            <?php $unPraticien['PRA_ADRESSE']." ".$unPraticien['PRA_CP']." ".$unPraticien['PRA_VILLE'] ?>
+        </td>
+        <td>
+            <?php $unPraticien['PRA_COEFNOTORIETE'] ?>
+        </td>
+        <td>
+            <?php $unPraticien['TYP_CODE'] ?>
+        </td>
+    </tr>
+<?php   
+    }
+?>
