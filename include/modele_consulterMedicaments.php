bc6e3ebf4dc7fe86820eecfbec538f0ad92f0b4b
Maxdweb <deleuil.maxime@gmail.com>
2013-03-19 12:32:04 +0100

    UPDATE: champ famille

--- include/modele_consulterMedicaments.php
+++ include/modele_consulterMedicaments.php
@@ -10,7 +10,7 @@ $myMedic = $pdo->getMedicById($_POST['id']);
 <medicament>
 	<depot><?php echo (isset($myMedic[0]['MED_DEPOTLEGAL'])) ? $myMedic[0]['MED_DEPOTLEGAL'] : "null" ; ?></depot>
 	<nom><?php echo (isset($myMedic[0]['MED_NOMCOMMERCIAL'])) ? $myMedic[0]['MED_NOMCOMMERCIAL'] : "null"; ?></nom>
-	<famille><?php echo (isset($myMedic[0]['FAM_CODE'])) ? $myMedic[0]['FAM_CODE'] : ""; ?></famille>
+	<famille><?php echo (isset($myMedic[0]['FAM_LIBELLE'])) ? $myMedic[0]['FAM_LIBELLE'] : ""; ?></famille>
 	<compo><?php echo (isset($myMedic[0]['MED_COMPOSITION'])) ? $myMedic[0]['MED_COMPOSITION'] : "null"; ?></compo>
 	<effet><?php echo (isset($myMedic[0]['MED_EFFETS'])) ? $myMedic[0]['MED_EFFETS'] : "null"; ?></effet>
 	<indic><?php echo (isset($myMedic[0]['MED_CONTREINDIC'])) ? $myMedic[0]['MED_CONTREINDIC'] : "null"; ?></indic>
