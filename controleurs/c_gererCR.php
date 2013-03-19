5404d0987f40c707485ea39656fd9496105a5742
Maxdweb <deleuil.maxime@gmail.com>
2013-03-19 14:42:19 +0100

    UPDATE: controleur gererCR

--- controleurs/c_gererCR.php
+++ controleurs/c_gererCR.php
@@ -1,9 +1,31 @@
 ﻿<?php
 include("vues/v_sommaire.php");
+
 $action = $_REQUEST['action'];
 $idVisiteur = $_SESSION['vis_matricule'];
-switch($action){
-	
+$praticiens = $pdo->getTousPraticiens("PRA_NOM");
+
+switch($action)
+{
+	case "saisirCR" :
+	{
+		include("vues/v_gererCR.php");
 	}
-include("vues/v_gererCR.php");
+	case "valideCR" :
+	{
+		include('modele_valideSasieCR.php');
+		if($_REQUEST['erreurs'])
+			include('vues/v_erreurs.php');
+		else
+		{
+			// INSERTION A L'AIDE DE LA FONCTION
+			include('vues/v_gererCR.php');
+		}
+	}
+	default :
+	{
+		include('vues/v_gererCR.php');	
+	}
+}
+
 ?>
