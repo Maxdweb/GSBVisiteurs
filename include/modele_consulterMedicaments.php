318b79199e5c16ed2434159c0cf2691d0d231f9b
Maxdweb <deleuil.maxime@gmail.com>
2013-03-29 10:10:18 +0100

    UPDATE: mod consult medics -> encodage

--- include/modele_consulterMedicaments.php
+++ include/modele_consulterMedicaments.php
@@ -1,3 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
 <?php
 header('content-type:text/xml');
 
@@ -6,7 +7,7 @@ $pdo = PdoGsb::getPdoGsb();
 
 $myMedic = $pdo->getMedicById($_POST['id']);
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
+
 <medicament>
 	<depot><?php echo (isset($myMedic[0]['MED_DEPOTLEGAL'])) ? $myMedic[0]['MED_DEPOTLEGAL'] : "null" ; ?></depot>
 	<nom><?php echo (isset($myMedic[0]['MED_NOMCOMMERCIAL'])) ? $myMedic[0]['MED_NOMCOMMERCIAL'] : "null"; ?></nom>
