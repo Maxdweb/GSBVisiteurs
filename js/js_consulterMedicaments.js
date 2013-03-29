da8fcc38fc4a80243fafdd1d4d3944a0558f8afd
Maxdweb <deleuil.maxime@gmail.com>
2013-03-29 10:08:26 +0100

    UPDATE: js

--- js/js_consulterMedicaments.js
+++ js/js_consulterMedicaments.js
@@ -21,10 +21,8 @@ function getXHR()
 }
 
 function request() {
-    var parser=new DOMParser();
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-            alert(parser.parseFromString(xhr.responseText,'text/xml').documentElement.innerText);
         	bindXHRxml(xhr.responseXML);
         }
     };
